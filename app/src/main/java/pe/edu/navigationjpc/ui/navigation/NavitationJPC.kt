d806b81312e4089d801b3b574f4a2ac737efa2c5
abdul8377 <abdulalfonso77@gmail.com>
2025-03-31 12:40:27 -0500

    asdf

---
+++ app/src/main/java/pe/edu/navigationjpc/ui/navigation/NavitationJPC.kt
@@ -0,0 +1,18 @@
+package pe.edu.navigationjpc.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import pe.edu.navigationjpc.ui.presentation.screens.HomeScreen
+import pe.edu.navigationjpc.ui.presentation.screens.ProfileScreen
+import pe.edu.navigationjpc.ui.presentation.screens.SettingsScreen
+
+@Composable
+fun NavigationHost(navController: NavHostController) {
+    NavHost(navController, startDestination = "home") {
+        composable("home") { HomeScreen() }
+        composable("profile") { ProfileScreen() }
+        composable("settings") { SettingsScreen() }
+    }
+}
